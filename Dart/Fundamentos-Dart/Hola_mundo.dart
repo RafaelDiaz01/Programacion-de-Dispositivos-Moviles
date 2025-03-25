77680546456dab0f47606d561cfcdcb7898db58f
Rafael Díaz López <kevinrdiaz98@gmail.com>
2025-03-25 16:55:40 -0600

    Se subieron los programas de fundamentos desarrollados en lenguaje Dart

---
+++ Dart/Fundamentos-Dart/Hola_mundo.dart
@@ -0,0 +1,5 @@
+void main(){
+  String myName = 'Mundo';
+  
+  print('Hola $myName');
+}
---
+++ Dart/Fundamentos-Dart/Variables.dart
@@ -0,0 +1,16 @@
+// Kevin Rafael Díaz López - 25/03/2025
+// Programa que muestra el uso de algunas variables en Dart.
+
+void main() {
+  final String pokemon = 'charmander';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List <String> abilities = ["impostor"];
+  
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  """);
+}
