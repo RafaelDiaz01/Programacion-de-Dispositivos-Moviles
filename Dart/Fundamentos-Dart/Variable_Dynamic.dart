e9dca5c4cad33b2eb4212826dde414b5979b0572
Rafael Díaz López <kevinrdiaz98@gmail.com>
2025-03-25 17:24:19 -0600

    Programa de variable dynamic en dart

---
+++ Dart/Fundamentos-Dart/Variable_Dynamic.dart
@@ -0,0 +1,11 @@
+// Kevin Rafael Díaz López - 25/03/2025
+// En una variable dynamic, el tipo puede cambiar cuando se use esta misma variable declarada con otros tipos de datos.
+// Debemos de tener cuidado al usar este tipo de variables.
+
+void main (){
+  dynamic errorMessage = 'Hola'; // String
+  errorMessage = true; // Booleano
+  errorMessage = [1,2,3]; // Lista
+  errorMessage = () => true; // Función que devuelve un booleano
+  errorMessage = null; // Nulo
+}
