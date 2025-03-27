5887d7b3c72ee9851fad5b636abebc21d63b413c
Rafael Díaz López <kevinrdiaz98@gmail.com>
2025-03-27 16:05:40 -0600

    Se creó el programa sobre el nombre de los constructores.

---
+++ Dart/Fundamentos-Dart/Name_constructors.dart
@@ -0,0 +1,42 @@
+// Kevin Rafael Díaz López - 27/03/2025
+// Name constructors en Dart.
+
+void main (){
+
+  final Map <String, dynamic> rawJson = {
+    'name' : 'Batman',
+    'power' : 'Money',
+    'car' : 'Batimovil'   
+  };
+
+  // Llamamos a nuestro constructor nombrado.
+  final ironman = Hero.fromJson(rawJson);
+
+  print('Personaje: $ironman');
+  
+}
+
+class Hero {
+  String name;
+  String power;
+  String car;
+
+  Hero({
+    required this.name,
+    required this.power,
+    required this.car
+  });
+
+  // Nombramos el constructor
+  Hero.fromJson ( Map <String,dynamic> json)
+    : name = json ['name'] ?? 'No name found',
+      power = json ['power'] ?? 'No power found',
+      car = json ['car'] ?? 'No car found';
+
+
+  // Sobreescribir el método toString para imprimir los datos en consola.
+  @override
+  String toString() {
+    return '$name, $power, $car'; // Se usa un ternario para verificar si esta vivo o o.
+  }
+}
