9b794632bfbc820b45ff0a3cda73e527f390a375
Rafael Díaz López <kevinrdiaz98@gmail.com>
2025-05-12 12:41:56 -0600

    Código Main comentado perfectamente

--- PRIMER_PARCIAL/girar_foto/lib/main.dart
+++ PRIMER_PARCIAL/girar_foto/lib/main.dart
@@ -1,62 +1,67 @@
+// Kevin Rafael Díaz López - 08/05/2024
+// Archivo principal de la aplicación QuizApp.
+
 import 'package:flutter/material.dart';
 import 'package:girar_foto/questions.dart';
 import 'package:girar_foto/quiz_body.dart';
 import 'package:girar_foto/quiz_results.dart';
 
-void main() {
+void main() { 
   runApp(MyApp());
 }
 
+// Clase principal de la aplicación.
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState(); // Estado de la aplicación, ya que es StatefulWidget.
 }
 
 class _MyAppState extends State<MyApp> {
-  /* Widget? currentScreen; Variable de tipo Widget para intercambiar entre pantallas.*/
-  String currentScreen = "home-screen";
+  String currentScreen = "home-screen"; // Guardamos en un String la palabra que utilizamos para ir a la pantalla inicial de la aplicación.
   final List<String> selectedAnswers = []; // Lista para guardar las respuestas seleccionadas.
-
-  /* Método para darle prioridad a alguna pantalla
-  @override
-  void initState() {
-    currentScreen = MyHomePage(startQuiz: switchScreen);
-
-    super.initState();
-  } */
 
   // Función para actualizar el estado y poder cambiar la pantalla.
   void switchScreen() {
     setState(() {
-      currentScreen = "question-screen";
-      selectedAnswers.clear();
+      currentScreen = "question-screen"; // Pantalla de preguntas.
+      selectedAnswers.clear(); // Reiniciamos la lista de respuestas seleccionadas.
+      // Esto es para que al regresar a la pantalla de preguntas, se reinicie la lista de respuestas seleccionadas.
     });
   }
 
+  // Función para agregar la respuesta seleccionada a la lista de respuestas.
+  // Esta función se pasa como parámetro a la pantalla de preguntas.
   void addSelectedAnswer(String answer) {
-    selectedAnswers.add(answer);
-    //print(selectedAnswers); Imprime en consola la lista de respuestas seleccionadas.
-    if (selectedAnswers.length == listQuestions.length) {
+    selectedAnswers.add(answer); // Agregamos la respuesta seleccionada a la lista de respuestas.
+    if (selectedAnswers.length == listQuestions.length) { // Si la longitud de la lista de respuestas es igual a la longitud de la lista de preguntas, significa que ya se respondieron todas las preguntas.
+      // Entonces cambiamos la pantalla a la pantalla de resultados.
       setState(() {
-        currentScreen = "results-screen";
+        currentScreen = "results-screen"; // Pantalla de resultados.
       });
     }
   }
 
-  // Función para regresar al inicio del Quiz
+  // Función para regresar al inicio del Quiz.
   void home(){
     setState(() {
-      currentScreen = "home-screen";
-      selectedAnswers.clear();
+      currentScreen = "home-screen"; // Pantalla de inicio.
+      selectedAnswers.clear(); // Reiniciamos la lista de respuestas seleccionadas.
     });
   }
 
+  // Método build que construye la interfaz de la aplicación.
+  // Este método se llama cada vez que se actualiza el estado de la aplicación.
+  // En nuestro caso, se llama cada vez que se cambia la pantalla.
   @override
   Widget build(BuildContext context) {
-    Widget screen = MyHomePage(startQuiz: switchScreen);
-    if (currentScreen == "question-screen") {
+
+    Widget screen = MyHomePage(startQuiz: switchScreen);     // Variable que guarda la pantalla que se va a mostrar, dependiendo del valor de currentScreen, se mostrará una pantalla u otra.
+    
+    if (currentScreen == "question-screen") { // Si la pantalla actual es la de preguntas, entonces se muestra la pantalla de preguntas.
+      // Se pasa la función addSelectedAnswer como parámetro a la pantalla de preguntas.
+      // Esta función se utiliza para agregar la respuesta seleccionada a la lista de respuestas.
       screen = QuizBody(addSelectedAnswer: addSelectedAnswer);
     } 
 
@@ -76,7 +81,7 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
-      home: screen, // screen debe estar
+      home: screen,
     );
   }
 }
